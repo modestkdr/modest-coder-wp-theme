84bc189a3bb3087ddb3a9786fa038b89850066a9
Srikanth AD <adsrikanth@gmail.com>
2015-07-04 22:50:49 -0400

    updated homepage template

--- homepage.php
+++ homepage.php
@@ -7,11 +7,34 @@ Template Name: Homepage
 <?php get_header();?>
 
 <div id="content-container" class="col-md-offset-1 col-md-10">
-<div class="page-content">
+<div class="page-content homepage-content">
+		<div class="col-md-12 latest-article">
+			<?php $cat_id = 2;
+$latest_cat_post = new WP_Query(array('posts_per_page' => 1, 'category__in' => array($cat_id)));
+if ($latest_cat_post->have_posts()): while ($latest_cat_post->have_posts()): $latest_cat_post->the_post();?>
 
-<p>Web developer passionate about developing and optimizing websites for better user experience and
-search engine visibility. Particularly interested in adapting content management systems for developing
-structured and scalable websites. </p>
+																																															<h1><a href="<?php the_permalink()?>">
+																																															<?php the_title();?></a></h1>
+																																															<p class="latest-post-date"><?php the_date();?></p>
+
+																																															<?php the_content();?>
+
+																																															<?php endwhile;endif;?>
+		</div>
+
+	<div class="container-fluid projects">
+			<h3 class="grey">Projects</h3>
+
+			<?php $cat_id = 8;
+$latest_cat_post = new WP_Query(array('posts_per_page' => 3, 'category__in' => array($cat_id)));
+if ($latest_cat_post->have_posts()): while ($latest_cat_post->have_posts()): $latest_cat_post->the_post();?>
+				<div class="col-md-4 project-card">
+														<h4><a href="<?php the_permalink()?>">
+														<?php the_title();?></a></h4>
+								<a href="<?php the_permalink()?>"><?php the_post_thumbnail();?></a></div>
+
+										<?php endwhile;endif;?>
+</div>
 </div>
 </div>
 
--- single.php
+++ single.php
@@ -2,24 +2,24 @@
 
 <div id="content-container" class="col-md-offset-1 col-md-10">
 <?php if (have_posts()): while (have_posts()): the_post();?>
-													<div class="row post-header">
-														<div class="col-md-10">
-															<h1 id="post-<?php the_ID();?>">
-																<?php the_title();?></h1></div>
-														<div class="col-md-2">
-															<p class="post-date"><?php echo get_the_date();?></p>
-														</div>
-													</div>
+															<div class="row post-header">
 																<div class="col-md-10">
-																<div class="post-content">
-																	<?php the_content('Read the rest of this entry &raquo;');?>
+																	<h1 id="post-<?php the_ID();?>">
+																		<?php the_title();?></h1></div>
+																<div class="col-md-2">
+																	<p class="post-date"><?php echo get_the_date();?></p>
 																</div>
+															</div>
+																		<div class="col-md-12">
+																		<div class="post-content">
+																			<?php the_content('Read the rest of this entry &raquo;');?>
+																		</div>
 
-																<div class="navigation group">
-																    <div class="alignleft"><?php previous_post_link('&laquo; %link')?></div>
-																    <div class="alignright"><?php next_post_link('%link &raquo;')?></div>
-																</div>
-																<?php endwhile;else: ?>
+																		<div class="navigation group">
+																		    <div class="alignleft"><?php previous_post_link('&laquo; %link')?></div>
+																		    <div class="alignright"><?php next_post_link('%link &raquo;')?></div>
+																		</div>
+																		<?php endwhile;else: ?>
 
 		<div class="warning">
 			<p>Sorry, but you are looking for something that isn't here.</p>
