7d55aaaaaacae92c5e384ef87431204ba016bcca
Srikanth AD <adsrikanth@gmail.com>
2015-07-05 00:57:22 -0400

    changed date format

--- single.php
+++ single.php
@@ -2,28 +2,28 @@
 
 <div id="content-container" class="col-md-offset-1 col-md-10">
 <?php if (have_posts()): while (have_posts()): the_post();?>
-																									<div class="row post-header">
-																										<div class="col-md-10">
-																											<h1 id="post-<?php the_ID();?>">
-																												<?php the_title();?></h1></div>
+																													<div class="row post-header">
+																														<div class="col-md-10">
+																															<h1 id="post-<?php the_ID();?>">
+																																<?php the_title();?></h1></div>
 
-																										<div class="col-md-2">
-																											<p class="post-date"><?php echo get_the_date();?></p>
-																										</div>
-																									</div>
-																												<div class="col-md-12">
-																												<div class="post-content">
-																												<p>
-																												<a href="https://twitter.com/share" class="twitter-share-button"
-																												data-via="Srikanth_AD" data-dnt="true">Tweet</a></p>
-																													<?php the_content('Read the rest of this entry &raquo;');?>
-																												</div>
+																														<div class="col-md-2">
+																															<p class="post-date"><?php echo get_the_date('M j, Y');?></p>
+																														</div>
+																													</div>
+																																<div class="col-md-12">
+																																<div class="post-content">
+																																<p>
+																																<a href="https://twitter.com/share" class="twitter-share-button"
+																																data-via="Srikanth_AD" data-dnt="true">Tweet</a></p>
+																																	<?php the_content('Read the rest of this entry &raquo;');?>
+																																</div>
 
-																												<div class="navigation group">
-																												    <div class="alignleft"><?php previous_post_link('&laquo; %link')?></div>
-																												    <div class="alignright"><?php next_post_link('%link &raquo;')?></div>
-																												</div>
-																												<?php endwhile;else: ?>
+																																<div class="navigation group">
+																																    <div class="alignleft"><?php previous_post_link('&laquo; %link')?></div>
+																																    <div class="alignright"><?php next_post_link('%link &raquo;')?></div>
+																																</div>
+																																<?php endwhile;else: ?>
 
 		<div class="warning">
 			<p>Sorry, but you are looking for something that isn't here.</p>
