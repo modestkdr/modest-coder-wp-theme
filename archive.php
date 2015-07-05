5b6da6ac103d0724cfb6039b52ce56e1e7b442db
Srikanth AD <adsrikanth@gmail.com>
2015-07-04 23:08:49 -0400

    updated archive template to display  projects

--- archive.php
+++ archive.php
@@ -3,7 +3,17 @@
 <div id="content-container" class="post-list col-md-offset-1 col-md-10">
 
 <?php if (is_category('Projects') && have_posts()): ?>
-	projects
+
+	<?php $cat_id = 8;
+$latest_cat_post = new WP_Query(array('posts_per_page' => 5, 'category__in' => array($cat_id)));
+if ($latest_cat_post->have_posts()): while ($latest_cat_post->have_posts()): $latest_cat_post->the_post();?>
+										<div class="col-md-4 project-card">
+																				<h4><a href="<?php the_permalink()?>">
+																				<?php the_title();?></a></h4>
+														<a href="<?php the_permalink()?>"><?php the_post_thumbnail();?></a></div>
+
+																<?php endwhile;endif;?>
+
 <?php endif;?>
 
 <?php if (have_posts() && !is_category('Projects')): ?>
@@ -11,25 +21,22 @@
 <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 
 <?php while (have_posts()): the_post();?>
-						<article>
-							<div class="col-md-3">
+									<article>
+										<div class="col-md-3">
 
-															</div>
-															<div class="col-md-9">
-																<h2 id="post-<?php the_ID();?>"><a href="<?php the_permalink()?>" rel="bookmark"><?php the_title();?></a></h2>
-																<p class="post-list-date"><?php echo get_the_date();?></p>
-																		<div class="main">
-																			<?php the_content(' ... read more');?>
 																		</div>
-															</div>
-						</article>
+																		<div class="col-md-9">
+																			<h2 id="post-<?php the_ID();?>"><a href="<?php the_permalink()?>" rel="bookmark"><?php the_title();?></a></h2>
+																			<p class="post-list-date"><?php echo get_the_date();?></p>
+																					<div class="main">
+																						<?php the_content(' ... read more');?>
+																					</div>
+																		</div>
+									</article>
 
 
 
-																		<?php endwhile;else: ?>
-<div class="warning">
-	<p>Sorry, but you are looking for something that isn't here.</p>
-</div>
+																					<?php endwhile;else: ?>
 <?php endif;?>
 
 <div class="navigation group">
