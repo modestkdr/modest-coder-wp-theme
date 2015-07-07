3e932ba455ca6d1e66ee4e95842bea380b8cc3f5
Srikanth AD <adsrikanth@gmail.com>
2015-07-06 22:08:12 -0400

    updated projects link in homepage

--- homepage.php
+++ homepage.php
@@ -7,38 +7,38 @@ Template Name: Homepage
 <?php get_header();?>
 
 <div id="content-container" class="col-md-offset-1 col-md-10">
-<div class="page-content homepage-content">
+	<div class="page-content homepage-content">
 		<div class="col-md-12 latest-article">
 			<?php $cat_id = 2;
 $latest_cat_post = new WP_Query(array('posts_per_page' => 1, 'category__in' => array($cat_id)));
 if ($latest_cat_post->have_posts()): while ($latest_cat_post->have_posts()): $latest_cat_post->the_post();?>
 
-																																																													<h1><a href="<?php the_permalink()?>">
-																																																													<?php the_title();?></a></h1>
-																																																													<p class="latest-post-date"><?php the_date();?></p>
+					<h1><a href="<?php the_permalink()?>">
+						<?php the_title();?></a></h1>
+						<p class="latest-post-date"><?php the_date();?></p>
 
-																																																													<?php the_content();?>
+						<?php the_content();?>
 
-																																																													<?php endwhile;endif;?>
+					<?php endwhile;endif;?>
 		</div>
 
-	<div class="container-fluid projects">
-			<h3 class="grey">Projects</h3>
+		<div class="container-fluid projects">
+			<h3><a href="/type/projects"><span class="grey">Projects</span></a></h3>
 
 			<?php $cat_id = 8;
 $latest_cat_post = new WP_Query(array('posts_per_page' => 3, 'category__in' => array($cat_id)));
 if ($latest_cat_post->have_posts()): while ($latest_cat_post->have_posts()): $latest_cat_post->the_post();?>
-																		<div class="col-md-4 project-card">
-																												<h4><a href="<?php the_permalink()?>">
-																												<?php the_title();?></a></h4>
-																						<a href="<?php the_permalink()?>">
-																						<?php the_post_thumbnail('attachment-thumbnail', array('class' => 'img-responsive'));?>
-																						</a></div>
+					<div class="col-md-4 project-card">
+						<h4><a href="<?php the_permalink()?>">
+							<?php the_title();?></a></h4>
+							<a href="<?php the_permalink()?>">
+								<?php the_post_thumbnail('attachment-thumbnail', array('class' => 'img-responsive'));?>
+							</a></div>
 
-																								<?php endwhile;endif;?>
-</div>
-</div>
-</div>
+						<?php endwhile;endif;?>
+			</div>
+		</div>
+	</div>
 
 
-<?php get_footer();?>
+	<?php get_footer();?>
